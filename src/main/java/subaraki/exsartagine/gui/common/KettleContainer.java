6442c24f0658cbc4038ea696357e5a2da883ef3b
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2021-01-07 01:43:18 -0500

    shift clicking is annoying

--- src/main/java/subaraki/exsartagine/gui/common/KettleContainer.java
+++ src/main/java/subaraki/exsartagine/gui/common/KettleContainer.java
@@ -10,20 +10,20 @@ import subaraki.exsartagine.tileentity.KettleBlockEntity;
 
 public class KettleContainer extends Container {
 
-	public KettleContainer(InventoryPlayer playerInventory, KettleBlockEntity pot) {
+    public KettleContainer(InventoryPlayer playerInventory, KettleBlockEntity pot) {
 
-      this.addSlotToContainer(new SlotItemHandler(pot.handler, 0, 15, 35));
-      for (int x = 0; x < 3; x++) {
-          for (int y = 0; y < 3; y++) {
-              this.addSlotToContainer(new SlotItemHandler(pot.handler, x + 3 * y + 1, 33 + 18 * x, 17 + 18 * y));
-          }
-      }
+        this.addSlotToContainer(new SlotItemHandler(pot.handler, 0, 15, 35));
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                this.addSlotToContainer(new SlotItemHandler(pot.handler, x + 3 * y + 1, 33 + 18 * x, 17 + 18 * y));
+            }
+        }
 
-      for (int x = 0; x < 3; x++) {
-          for (int y = 0; y < 3; y++) {
-              this.addSlotToContainer(new SlotOutput(playerInventory.player,pot.handler, x + 3 * y + 10, 116 + 18 * x, 17 + 18 * y));
-          }
-      }
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                this.addSlotToContainer(new SlotOutput(playerInventory.player, pot.handler, x + 3 * y + 10, 116 + 18 * x, 17 + 18 * y));
+            }
+        }
 
         for (int i = 0; i < 3; ++i)
             for (int j = 0; j < 9; ++j)
@@ -31,58 +31,46 @@ public class KettleContainer extends Container {
 
         for (int k = 0; k < 9; ++k)
             this.addSlotToContainer(new Slot(playerInventory, k, 8 + k * 18, 142));
-	}
+    }
 
-	@Override
-	public boolean canInteractWith(EntityPlayer playerIn) {
-		return true;
-	}
-	
-	/**
+    @Override
+    public boolean canInteractWith(EntityPlayer playerIn) {
+        return true;
+    }
+
+    /**
      * Take a stack from the specified inventory slot.
      */
-    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index)
-    {
+    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
         ItemStack itemstack = ItemStack.EMPTY;
         Slot slot = this.inventorySlots.get(index);
 
-        if (slot != null && slot.getHasStack())
-        {
+        if (slot != null && slot.getHasStack()) {
             ItemStack itemstack1 = slot.getStack();
             itemstack = itemstack1.copy();
 
-            if (index == 1)
-            {
-                if (!this.mergeItemStack(itemstack1, 2, 38, true))
-                {
+            if (index == 0) {
+                if (!this.mergeItemStack(itemstack1, 19, this.inventorySlots.size(), false)) {
                     return ItemStack.EMPTY;
                 }
-
+            } else if (index < 19) {
+                if (!this.mergeItemStack(itemstack1, 19, this.inventorySlots.size(), true)) {
+                    return ItemStack.EMPTY;
+                }
                 slot.onSlotChange(itemstack1, itemstack);
-            }
-            else if (index != 0)
-            {
-                if (!this.mergeItemStack(itemstack1, 0, 1, false))
-                {
+            } else {
+                if (!this.mergeItemStack(itemstack1, 1, 10, false)) {
                     return ItemStack.EMPTY;
                 }
             }
-            else if (!this.mergeItemStack(itemstack1, 2, 38, false))
-            {
-                return ItemStack.EMPTY;
-            }
 
-            if (itemstack1.isEmpty())
-            {
+            if (itemstack1.isEmpty()) {
                 slot.putStack(ItemStack.EMPTY);
-            }
-            else
-            {
+            } else {
                 slot.onSlotChanged();
             }
 
-            if (itemstack1.getCount() == itemstack.getCount())
-            {
+            if (itemstack1.getCount() == itemstack.getCount()) {
                 return ItemStack.EMPTY;
             }
 
