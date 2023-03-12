a08f3e0b9977b919157ed7c49f22469e6343d7b1
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-03-12 18:03:49 -0400

    switch to using annotation config, move mod metadata to mcmod.info

--- src/main/java/subaraki/exsartagine/util/ConfigHandler.java
+++ src/main/java/subaraki/exsartagine/util/ConfigHandler.java
@@ -1,26 +1,13 @@
 package subaraki.exsartagine.util;
 
-import java.io.File;
+import net.minecraftforge.common.config.Config;
+import subaraki.exsartagine.ExSartagine;
 
-import net.minecraftforge.common.config.Configuration;
 
-public class ConfigHandler 
-{
-	public static ConfigHandler instance = new ConfigHandler();
+@Config(modid = ExSartagine.MODID)
+public class ConfigHandler {
 
+	@Config.Name("bonus_chance")
+	@Config.Comment("Define how often a bonus ingot will smelt in the Smelter. Expressed in percent. [0% - 100%]")
 	public static int percent = 33;
-
-	public void loadConfig(File file)
-	{
-		Configuration config = new Configuration(file);
-		config.load();
-		loadSettings(config);
-		config.save();
-	}
-
-	private void loadSettings(Configuration config)
-	{
-		config.addCustomCategoryComment("Bonus Slot", "Define how often a bonus ingot will smelt in the Smelter.");
-		config.getInt("bonus ingot", "Bonus Slot", 33, 0, 100, "Expressed in percent. [0% - 100%]");
-	}
 }
