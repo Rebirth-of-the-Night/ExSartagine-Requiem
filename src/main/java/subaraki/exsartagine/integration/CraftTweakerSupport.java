5faf483d1cbe7daf1562f46cfe6f16747898ad9d
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-05-22 13:31:09 -0400

    modify ct methods slightly, documentation

--- src/main/java/subaraki/exsartagine/integration/CraftTweakerSupport.java
+++ src/main/java/subaraki/exsartagine/integration/CraftTweakerSupport.java
@@ -33,20 +33,204 @@ import com.google.common.collect.Sets;
 @ZenClass("mods." + ExSartagine.MODID + ".ExSartagine")
 public class CraftTweakerSupport {
 
+
+    /**
+     * Adds a pot recipe
+     * @param input
+     * @param output
+     */
     @ZenMethod
     public static void addPotRecipe(IIngredient input, IItemStack output) {
         CraftTweakerAPI.apply(new AddPotAction(CraftTweakerMC.getIngredient(input), CraftTweakerMC.getItemStack(output)));
     }
-    
+
+    /**
+     * Removes all pot recipes producing the given output
+     * @param output the output to remove
+     */
     @ZenMethod
     public static void removePotRecipe(IItemStack output) {
         CraftTweakerAPI.apply(new RemovePotAction(CraftTweakerMC.getItemStack(output)));
     }
-    
+
+    /**
+     * Removes all pot recipes that specify the given output AND matching the input
+     * @param input the input to check
+     * @param output the output to remove
+     */
     @ZenMethod
     public static void removePotRecipe(IIngredient input, IItemStack output) {
         CraftTweakerAPI.apply(new RemovePotAction(CraftTweakerMC.getIngredient(input), CraftTweakerMC.getItemStack(output)));
     }
+
+    ///////////////////////////////
+
+    /**
+     * Adds wok recipe
+     * @param inputs up to 9 ingredients can be specified
+     * @param outputs up to 9 outputs can be specified
+     * @param flips flips required with `ore:spatula`, defaults to 0
+     */
+    @ZenMethod
+    public static void addWokRecipe(IIngredient[] inputs, IItemStack[] outputs,@Optional int flips) {
+        addWokRecipe(inputs,null,outputs,flips);
+    }
+
+    /**
+     * Adds wok recipe with liquid requirement
+     * @param inputs up to 9 ingredients can be specified
+     * @param liquid input liquid required for recipe
+     * @param outputs up to 9 outputs can be specified
+     * @param flips flips required with `ore:spatula`, defaults to 0
+     */
+    @ZenMethod
+    public static void addWokRecipe(IIngredient[] inputs,ILiquidStack liquid, IItemStack[] outputs,@Optional int flips) {
+
+        List<Ingredient> iinputs = Arrays.stream(inputs).map(CraftTweakerMC::getIngredient).collect(Collectors.toList());
+        FluidStack fluidStack = CraftTweakerMC.getLiquidStack(liquid);
+
+        List<ItemStack> iOutputs = Arrays.stream(outputs).map(CraftTweakerMC::getItemStack).collect(Collectors.toList());
+
+        CraftTweakerAPI.apply(new AddWokAction(iinputs,fluidStack, iOutputs,flips));
+    }
+
+    /**
+     * Removes wok recipe by name
+     * @param name the name of the recipe specified in tooltip
+     */
+    @ZenMethod
+    public static void removeWokRecipe(String name) {
+        CraftTweakerAPI.apply(new RemoveWokAction(new ResourceLocation(name)));
+    }
+
+    ////////////////////////////////////////////////////
+
+    /**
+     * Adds smelter recipe
+     * @param input
+     * @param output
+     */
+    @ZenMethod
+    public static void addSmelterRecipe(IIngredient input, IItemStack output) {
+        CraftTweakerAPI.apply(new AddSmelterAction(CraftTweakerMC.getIngredient(input), CraftTweakerMC.getItemStack(output)));
+    }
+
+    /**
+     * Removed all smelter recipes producing the given output
+     * @param output
+     */
+    @ZenMethod
+    public static void removeSmelterRecipe(IItemStack output) {
+        CraftTweakerAPI.apply(new RemoveSmelterAction(CraftTweakerMC.getItemStack(output)));
+    }
+
+    /**
+     * Removes all smelter recipes producing the given output AND matching the input
+     * @param input
+     * @param output
+     */
+    @ZenMethod
+    public static void removeSmelterRecipe(IIngredient input, IItemStack output) {
+        CraftTweakerAPI.apply(new RemoveSmelterAction(CraftTweakerMC.getIngredient(input), CraftTweakerMC.getItemStack(output)));
+    }
+
+
+    /**
+     * Adds kettle recipe
+     * @param inputs up to 9 ingredients can be specified
+     * @param outputs up to 9 outputs can be specified
+     * @param liquid input liquid required for recipe
+     * @param time cook time in ticks, defaults to 200 (10 seconds)
+     */
+    @ZenMethod
+    public static void addKettleRecipe(IIngredient[] inputs, ILiquidStack liquid,IItemStack[] outputs, @Optional("200") int time) {
+        addKettleRecipe(inputs, null, liquid, null, outputs, time);
+    }
+
+    /**
+     * Adds kettle recipe with liquid requirement
+     * @param inputs up to 9 ingredients can be specified
+     * @param liquid input liquid required for recipe
+     * @param outputs up to 9 outputs can be specified
+     * @param time cook time in ticks, defaults to 200 (10 seconds)
+     */
+    @ZenMethod
+    public static void addKettleRecipe(IIngredient[] inputs, IIngredient catalyst, ILiquidStack liquid,IItemStack[] outputs, @Optional("200") int time) {
+        addKettleRecipe(inputs, catalyst, liquid, null, outputs, time);
+    }
+
+    /**
+     * Adds kettle recipe with liquid requirement and liquid output
+     * @param inputs up to 9 ingredients can be specified
+     * @param catalyst catalyst item, not consumed by recipe
+     * @param liquidInput input liquid required for recipe
+     * @param outputs up to 9 outputs can be specified
+     * @param liquidOutput output liquid for recipe
+     * @param time cook time in ticks, defaults to 200 (10 seconds)
+     */
+    @ZenMethod
+    public static void addKettleRecipe(IIngredient[] inputs, IIngredient catalyst, ILiquidStack liquidInput,
+                                       ILiquidStack liquidOutput,IItemStack[] outputs, @Optional("200") int time) {
+        List<Ingredient> iinputs = Arrays.stream(inputs).map(CraftTweakerMC::getIngredient).collect(Collectors.toList());
+        Ingredient iCatalyst = new IIngredientWrapper(catalyst);
+        FluidStack iFluidInput = CraftTweakerMC.getLiquidStack(liquidInput);
+        FluidStack iFluidOutput = CraftTweakerMC.getLiquidStack(liquidOutput);
+        List<ItemStack> iOutputs = Arrays.stream(outputs).map(CraftTweakerMC::getItemStack).collect(Collectors.toList());
+        CraftTweakerAPI.apply(new AddKettleAction(iinputs, iCatalyst, iFluidInput,iFluidOutput, iOutputs, time));
+    }
+
+    /**
+     * Removes kettle recipe by name
+     * @param name the name of the recipe specified in tooltip
+     */
+    @ZenMethod
+    public static void removeKettleRecipe(String name) {
+        CraftTweakerAPI.apply(new RemoveKettleAction(new ResourceLocation(name)));
+    }
+
+    //////////////////////////////////////////////
+
+    /**
+     * Adds a block that kitchenware can be placed on
+     * @param state The blockstate to add
+     * @param heat whether this block can heat kitchenware
+     * @param legs whether legs should be placed below the kitchenware (default: false)
+     */
+    @ZenMethod
+    public static void addPlaceable(IBlockState state,boolean heat,@Optional boolean legs) {
+        CraftTweakerAPI.apply(new BlockStateAction(CraftTweakerMC.getBlockState(state), heat, true,legs));
+    }
+
+    /**
+     * Adds a block that kitchenware can be placed on
+     * @param block The block to add
+     * @param heat whether this block can heat kitchenware
+     * @param legs whether legs should be placed below the kitchenware (default: false)
+     */
+    @ZenMethod
+    public static void addPlaceable(IBlock block,boolean heat,@Optional boolean legs) {
+        CraftTweakerAPI.apply(new BlockStateAction(CraftTweakerMC.getBlock(block).getBlockState().getValidStates(), heat, true,legs));
+    }
+
+    /**
+     * Removes kitchenware placeable
+     * @param state The blockstate to remove
+     */
+    @ZenMethod
+    public static void removePlaceable(IBlockState state) {
+        CraftTweakerAPI.apply(new BlockStateAction(CraftTweakerMC.getBlockState(state), false, false,false));
+    }
+
+    /**
+     * Removes kitchenware placeable
+     * @param block The block to remove
+     */
+    @ZenMethod
+    public static void removePlaceable(IBlock block) {
+        CraftTweakerAPI.apply(new BlockStateAction(CraftTweakerMC.getBlock(block).getBlockState().getValidStates(), false, false,false));
+    }
+
+    //////////////////////////////////////////////////////////
 
     private static class AddPotAction implements IAction {
         private final Ingredient input;
@@ -97,113 +281,9 @@ public class CraftTweakerSupport {
                 done = ModRecipes.removePotRecipe(output);
             else
                 done = ModRecipes.removePotRecipe(input, output);
-            
+
             if (!done)
                 CraftTweakerAPI.logWarning("No pot recipes removed for input " + input + " and output " + output);
-        }
-    }
-
-
-    @ZenMethod
-    public static void addWokRecipe(IIngredient[] inputs, IItemStack[] outputs,@Optional int flips) {
-        addWokRecipe(inputs,null,outputs,flips);
-    }
-
-    @ZenMethod
-    public static void addWokRecipe(IIngredient[] inputs,ILiquidStack liquid, IItemStack[] outputs,@Optional int flips) {
-
-        List<Ingredient> iinputs = Arrays.stream(inputs).map(CraftTweakerMC::getIngredient).collect(Collectors.toList());
-        FluidStack fluidStack = CraftTweakerMC.getLiquidStack(liquid);
-
-        List<ItemStack> iOutputs = Arrays.stream(outputs).map(CraftTweakerMC::getItemStack).collect(Collectors.toList());
-
-        CraftTweakerAPI.apply(new AddWokAction(iinputs,fluidStack, iOutputs,flips));
-    }
-
-    @ZenMethod
-    public static void removeWokRecipe(String name) {
-        CraftTweakerAPI.apply(new RemoveWokAction(new ResourceLocation(name)));
-    }
-
-    private static class AddWokAction implements IAction {
-        private final List<Ingredient> inputs;
-        private final FluidStack fluid;
-        private final List<ItemStack> output;
-        private final int flips;
-
-        public AddWokAction(List<Ingredient> inputs, FluidStack fluid, List<ItemStack> output, int flips) {
-            this.inputs = inputs;
-            this.fluid = fluid;
-            this.output = output;
-            this.flips = flips;
-        }
-
-        @Override
-        public String describe() {
-            return "Adding wok recipe with input " + inputs;
-        }
-
-        @Override
-        public void apply() {
-            ModRecipes.addWokRecipe(inputs,fluid, output,flips);
-        }
-    }
-
-    private static class RemoveWokAction implements IAction {
-        private final ResourceLocation name;
-
-        public RemoveWokAction(ResourceLocation name) {
-            this.name = name;
-        }
-
-        @Override
-        public String describe() {
-                return "Removing wok recipe with name " + name;
-        }
-
-        @Override
-        public void apply() {
-            boolean done;
-                done = ModRecipes.removeWokRecipeByName(name);
-
-            if (!done) {
-                    CraftTweakerAPI.logWarning("No pot recipes removed for name " + name);
-            }
-        }
-    }
-
-    @ZenMethod
-    public static void addSmelterRecipe(IIngredient input, IItemStack output) {
-        CraftTweakerAPI.apply(new AddSmelterAction(CraftTweakerMC.getIngredient(input), CraftTweakerMC.getItemStack(output)));
-    }
-
-    @ZenMethod
-    public static void removeSmelterRecipe(IItemStack output) {
-        CraftTweakerAPI.apply(new RemoveSmelterAction(CraftTweakerMC.getItemStack(output)));
-    }
-
-    @ZenMethod
-    public static void removeSmelterRecipe(IIngredient input, IItemStack output) {
-        CraftTweakerAPI.apply(new RemoveSmelterAction(CraftTweakerMC.getIngredient(input), CraftTweakerMC.getItemStack(output)));
-    }
-
-    private static class AddSmelterAction implements IAction {
-        private final Ingredient input;
-        private final ItemStack output;
-
-        public AddSmelterAction(Ingredient input, ItemStack output) {
-            this.input = input;
-            this.output = output;
-        }
-
-        @Override
-        public String describe() {
-            return "Adding pot recipe with input " + input;
-        }
-
-        @Override
-        public void apply() {
-            ModRecipes.addSmelterRecipe(input, output);
         }
     }
 
@@ -246,26 +326,6 @@ public class CraftTweakerSupport {
         }
     }
 
-    @ZenMethod
-    public static void addKettleRecipe(IIngredient[] inputs, ILiquidStack fluidInput,IItemStack[] outputs, @Optional("200") int time) {
-        addKettleRecipe(inputs, null, fluidInput, null, outputs, time);
-    }
-    @ZenMethod
-    public static void addKettleRecipe(IIngredient[] inputs, IIngredient catalyst, ILiquidStack fluidInput,IItemStack[] outputs, @Optional("200") int time) {
-        addKettleRecipe(inputs, catalyst, fluidInput, null, outputs, time);
-    }
-
-    @ZenMethod
-    public static void addKettleRecipe(IIngredient[] inputs, IIngredient catalyst, ILiquidStack fluidInput,
-                                       ILiquidStack fluidOutput,IItemStack[] outputs, @Optional("200") int time) {
-        List<Ingredient> iinputs = Arrays.stream(inputs).map(CraftTweakerMC::getIngredient).collect(Collectors.toList());
-        Ingredient iCatalyst = new IIngredientWrapper(catalyst);
-        FluidStack iFluidInput = CraftTweakerMC.getLiquidStack(fluidInput);
-        FluidStack iFluidOutput = CraftTweakerMC.getLiquidStack(fluidOutput);
-        List<ItemStack> iOutputs = Arrays.stream(outputs).map(CraftTweakerMC::getItemStack).collect(Collectors.toList());
-        CraftTweakerAPI.apply(new AddKettleAction(iinputs, iCatalyst, iFluidInput,iFluidOutput, iOutputs, time));
-    }
-
     private static class AddKettleAction implements IAction {
         private final List<Ingredient> inputs;
         private final Ingredient catalyst;
@@ -289,8 +349,8 @@ public class CraftTweakerSupport {
                     this.fluidOutput == null ? "consumption" : "transformation" +
                     " recipe with inputs " + inputs + ", catalyst " +
                     catalyst + ", fluid input " + fluidInput + (this.fluidOutput == null ?
-                        "" :
-                        ", fluid output " + fluidOutput) +
+                    "" :
+                    ", fluid output " + fluidOutput) +
                     ", outputs " + outputs + ", and duration " + time;
         }
 
@@ -299,10 +359,29 @@ public class CraftTweakerSupport {
             ModRecipes.addKettleRecipe(inputs, catalyst, fluidInput, fluidOutput, outputs, time);
         }
     }
-    
-    @ZenMethod
-    public static void removeKettleRecipe(String name) {
-        CraftTweakerAPI.apply(new RemoveKettleAction(new ResourceLocation(name)));
+
+    private static class AddWokAction implements IAction {
+        private final List<Ingredient> inputs;
+        private final FluidStack fluid;
+        private final List<ItemStack> output;
+        private final int flips;
+
+        public AddWokAction(List<Ingredient> inputs, FluidStack fluid, List<ItemStack> output, int flips) {
+            this.inputs = inputs;
+            this.fluid = fluid;
+            this.output = output;
+            this.flips = flips;
+        }
+
+        @Override
+        public String describe() {
+            return "Adding wok recipe with input " + inputs;
+        }
+
+        @Override
+        public void apply() {
+            ModRecipes.addWokRecipe(inputs,fluid, output,flips);
+        }
     }
 
     private static class RemoveKettleAction implements IAction {
@@ -324,46 +403,6 @@ public class CraftTweakerSupport {
             if (!done)
                 CraftTweakerAPI.logWarning("No kettle recipes removed for name " + name);
         }
-    }
-
-    @ZenMethod
-    public static void addHeatSource(IBlockState source,@Optional boolean legs) {
-        CraftTweakerAPI.apply(new BlockStateAction(CraftTweakerMC.getBlockState(source), true, true,legs));
-    }
-
-    @ZenMethod
-    public static void addHeatSource(IBlock source,@Optional boolean legs) {
-        CraftTweakerAPI.apply(new BlockStateAction(CraftTweakerMC.getBlock(source).getBlockState().getValidStates(), true, true,legs));
-    }
-    
-    @ZenMethod
-    public static void removeHeatSource(IBlockState source,@Optional boolean legs) {
-        CraftTweakerAPI.apply(new BlockStateAction(CraftTweakerMC.getBlockState(source), true, false,legs));
-    }
-
-    @ZenMethod
-    public static void removeHeatSource(IBlock source,@Optional boolean legs) {
-        CraftTweakerAPI.apply(new BlockStateAction(CraftTweakerMC.getBlock(source).getBlockState().getValidStates(), true, false,legs));
-    }
-
-    @ZenMethod
-    public static void addPlaceable(IBlockState source,@Optional boolean legs) {
-        CraftTweakerAPI.apply(new BlockStateAction(CraftTweakerMC.getBlockState(source), false, true,legs));
-    }
-
-    @ZenMethod
-    public static void addPlaceable(IBlock source,@Optional boolean legs) {
-        CraftTweakerAPI.apply(new BlockStateAction(CraftTweakerMC.getBlock(source).getBlockState().getValidStates(), false, true,legs));
-    }
-    
-    @ZenMethod
-    public static void removePlaceable(IBlockState source,@Optional boolean legs) {
-        CraftTweakerAPI.apply(new BlockStateAction(CraftTweakerMC.getBlockState(source), false, false,legs));
-    }
-
-    @ZenMethod
-    public static void removePlaceable(IBlock source,@Optional boolean legs) {
-        CraftTweakerAPI.apply(new BlockStateAction(CraftTweakerMC.getBlock(source).getBlockState().getValidStates(), false, false,legs));
     }
 
     private static class BlockStateAction implements IAction {
@@ -403,6 +442,49 @@ public class CraftTweakerSupport {
                             (this.isHeatSource ? "heat sources " : "placeables ") +
                             "were removed for block state " + this.states);
             }
+        }
+    }
+
+    private static class RemoveWokAction implements IAction {
+        private final ResourceLocation name;
+
+        public RemoveWokAction(ResourceLocation name) {
+            this.name = name;
+        }
+
+        @Override
+        public String describe() {
+            return "Removing wok recipe with name " + name;
+        }
+
+        @Override
+        public void apply() {
+            boolean done;
+            done = ModRecipes.removeWokRecipeByName(name);
+
+            if (!done) {
+                CraftTweakerAPI.logWarning("No pot recipes removed for name " + name);
+            }
+        }
+    }
+
+    private static class AddSmelterAction implements IAction {
+        private final Ingredient input;
+        private final ItemStack output;
+
+        public AddSmelterAction(Ingredient input, ItemStack output) {
+            this.input = input;
+            this.output = output;
+        }
+
+        @Override
+        public String describe() {
+            return "Adding pot recipe with input " + input;
+        }
+
+        @Override
+        public void apply() {
+            ModRecipes.addSmelterRecipe(input, output);
         }
     }
 }
