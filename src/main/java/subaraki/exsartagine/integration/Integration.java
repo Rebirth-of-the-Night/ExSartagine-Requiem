c676e52d22287f8c2c50776a44acb6475f0588e9
H <37481947+H-QueerCoded@users.noreply.github.com>
2023-07-03 18:27:56 +1200

    Dependency changes & FutureMC crash fix

--- src/main/java/subaraki/exsartagine/integration/Integration.java
+++ src/main/java/subaraki/exsartagine/integration/Integration.java
@@ -1,8 +1,14 @@
 package subaraki.exsartagine.integration;
 
+import net.minecraft.block.Block;
 import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class Integration {
+
+    @GameRegistry.ObjectHolder("futuremc:campfire")
+    public static Block FUTUREMC_CAMPFIRE = null;
+
     public static void postInit(){
         if(Loader.isModLoaded("betterwithmods")){ BetterWithMods.addPlaceables(); }
         if(Loader.isModLoaded("pyrotech")){Pyrotech.addPlaceables();}
