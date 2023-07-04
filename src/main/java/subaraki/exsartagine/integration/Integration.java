ecedb2f8be76f5e15bd3f1058bd0f4af13fa0921
H <37481947+H-QueerCoded@users.noreply.github.com>
2023-07-04 16:35:35 +1200

    Engineer's Decor Lab furnace integration (#17)
    * Lab furnace integration
    
    * Update build.gradle
    
    ---------
    
    Co-authored-by: Colin Wong <cwongmath@gmail.com>

--- src/main/java/subaraki/exsartagine/integration/Integration.java
+++ src/main/java/subaraki/exsartagine/integration/Integration.java
@@ -15,5 +15,6 @@ public class Integration {
         if(Loader.isModLoaded("futuremc")){FutureMC.addPlaceables();}
         if(Loader.isModLoaded("campfire")){Campfire.addPlaceables();}
         if(Loader.isModLoaded("immersiveengineering")){ImmersiveEngineering.registerHeatableAdapters();}
+        if(Loader.isModLoaded("engineersdecor")){EngineersDecor.addPlaceables();}
     }
 }
